e4783045249b9541c1abc44049a964a977e5bc8e
topocai <leonjoserp@gmail.com>
2024-03-04 19:56:54 -0300

    testing

--- src/resources/image-analysis.js
+++ src/resources/image-analysis.js
@@ -2,8 +2,8 @@ const { ImageAnalysisClient } = require('@azure-rest/ai-vision-image-analysis');
 const createClient = require('@azure-rest/ai-vision-image-analysis').default;
 const { AzureKeyCredential } = require('@azure/core-auth');
 
-const endpoint = process.env.REACT_APP_VISION_ENDPOINT || process.env.VISION_ENDPOINT;
-const key = process.env.REACT_APP_VISION_KEY || process.env.VISION_KEY;
+//const endpoint = process.env.REACT_APP_VISION_ENDPOINT || process.env.VISION_ENDPOINT;
+//const key = process.env.REACT_APP_VISION_KEY || process.env.VISION_KEY;
 
 const credential = new AzureKeyCredential(key);
 const client = createClient(endpoint, credential);
